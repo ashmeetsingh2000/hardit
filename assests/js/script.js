cf5d0593de5317e7f9eb8a0c23c9634727bed231
ashmeet-soforix <ashmeet.singh@soforix.com>
2026-07-27 05:27:00 +0530

    something new

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,3 +1,4 @@
+// document.body.classList.add("no-scroll");
 const loader = document.getElementById("loader");
 const stamp = document.getElementById("loaderStamp");
 const stampText = document.getElementById("loaderStampText");
@@ -24,13 +25,22 @@ function startScrollAnimations() {
         .forEach(element => observer.observe(element));
 
 }
+function animateFirstFlowerGarden() {
+
+    document
+        .querySelectorAll("#firstFlowerGarden .scale-fade-in")
+        .forEach(el => el.classList.add("show"));
+
+}
 
 function openEnvelope() {
+
     window.scrollTo({
         top: 0,
         left: 0,
         behavior: "instant"
     });
+
     loader.classList.add("open");
 
     setTimeout(() => {
@@ -40,10 +50,70 @@ function openEnvelope() {
     setTimeout(() => {
 
         loader.classList.add("finished");
+        document.body.classList.remove("no-scroll");
+
         startScrollAnimations();
+        animateFirstFlowerGarden();
 
     }, 3000);
 
 }
 stamp.addEventListener("click", openEnvelope);
 stampText.addEventListener("click", openEnvelope);
+
+// ======================================= remove this ===============================================
+startScrollAnimations() 
+// ======================================= remove this ===============================================
+
+
+const targetDate = new Date("November 14, 2026 00:00:00").getTime();
+
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+function setValue(element, value){
+
+    if(element.textContent === value) return;
+
+    element.classList.remove("flip");
+
+    void element.offsetWidth;
+
+    element.textContent = value;
+    element.classList.add("flip");
+
+}
+
+function updateCountdown(){
+
+    const now = Date.now();
+    const distance = targetDate - now;
+
+    if(distance <= 0){
+
+        setValue(daysEl,"00");
+        setValue(hoursEl,"00");
+        setValue(minutesEl,"00");
+        setValue(secondsEl,"00");
+
+        clearInterval(timer);
+        return;
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((distance / (1000 * 60)) % 60);
+    const seconds = Math.floor((distance / 1000) % 60);
+
+    setValue(daysEl, String(days).padStart(2,"0"));
+    setValue(hoursEl, String(hours).padStart(2,"0"));
+    setValue(minutesEl, String(minutes).padStart(2,"0"));
+    setValue(secondsEl, String(seconds).padStart(2,"0"));
+
+}
+
+updateCountdown();
+
+const timer = setInterval(updateCountdown,1000);
